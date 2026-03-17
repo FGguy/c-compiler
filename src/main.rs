1bcab91d0294e7650ee5c4f1900f35d111c7a848
Yan p <y.pilon9@gmail.com>
2026-03-17 00:42:01 -0400

    feat: integrate clap
    setup compiler cli stub with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
+/// Minimal C compiler written in Rust
+#[derive(Parser, Debug)]
+#[command(
+    name = "ycc",
+    version, 
+    about = "Yan's C compiler", 
+    long_about = "You really shouldn't use my mediocre C compiler lol.")]
+struct Args {
+    /// Path to C source file to compile
+    #[arg(short, long, required = true)]
+    input_filepath: String,
+
+    /// What to name the produced binary
+    #[arg(short, long, default_value_t = String::from("forgortonamefile"))]
+    name: String,
+
+    /// Where the produced binary is to be stored
+    #[arg(short, long, default_value_t = String::from("."))]
+    output_filepath: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Parsed arguments: {:?}", args);
 }
